0832c7d9e363e7c894b39cd091e62bb6267ecf8c
thailisg <thai.ori.gon.diaz@gmail.com>
2025-11-08 04:07:02 -0300

    queue Activity

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: here we add three elements to dequeue them all
+    // Expected Result: the elements are returned in order of highest to lowest priority
+    // Defect(s) Found: It does not return the elements in the expected order
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Low", 1);
+        priorityQueue.Enqueue("High", 3);
+        priorityQueue.Enqueue("Medium", 2);
+
+        Assert.AreEqual("High", priorityQueue.Dequeue(), "element with highest priority");
+        Assert.AreEqual("Medium", priorityQueue.Dequeue(), "element with medium priority");
+        Assert.AreEqual("Low", priorityQueue.Dequeue(), "element with lowest priority");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: add elements with the same priority
+    // Expected Result: that the elements return ordered in a queue
+    // Defect(s) Found: returns the last element instead of the first (they have the same priority)
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Thai", 1);
+        priorityQueue.Enqueue("Osman", 1);
+        priorityQueue.Enqueue("Silvia", 1);
+
+        Assert.AreEqual("Thai", priorityQueue.Dequeue());
+        Assert.AreEqual("Osman", priorityQueue.Dequeue());
+        Assert.AreEqual("Silvia", priorityQueue.Dequeue());
     }
 
     // Add more test cases as needed below.
+
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -37,17 +37,25 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
+
+        Person person = _people.Dequeue();
+
+        if (person.Turns <= 0)
+        {
+            _people.Enqueue(person);
+        }
+
         else
         {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            person.Turns--;
+            if (person.Turns > 0)
             {
-                person.Turns -= 1;
                 _people.Enqueue(person);
             }
-
-            return person;
         }
+
+        return person;
+        
     }
 
     public override string ToString()
